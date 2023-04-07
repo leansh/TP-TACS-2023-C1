b7def3b6ac70b73b9c81eeac086d35e834287acd
Leandro Shokida <leandroshokida@gmail.com>
2023-04-07 17:54:57 -0300

    Initial commit

---
+++ src/main/java/ar/edu/utn/frba/tacs/TpTacs2023C1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.tacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTacs2023C1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTacs2023C1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frba/tacs/TpTacs2023C1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.tacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTacs2023C1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
